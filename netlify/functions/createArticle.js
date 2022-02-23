5998caa0d598066da60ea50973f85a3ca409b21a
Dawidx3p <maildawcio@gmail.com>
2022-02-23 21:42:07 +0100

    add netlify function

---
+++ netlify/functions/createArticle.js
@@ -0,0 +1,30 @@
+/* Import faunaDB sdk */
+const faunadb = require('faunadb')
+const q = faunadb.query
+
+exports.handler = (event, context) => {
+  console.log('creating test data')
+  /* configure faunaDB Client with our secret */
+  const client = new faunadb.Client({
+    secret: process.env.REACT_APP_FAUNADB_ADMIN_SECRET
+  }) 
+  return client.query(
+    q.Create(
+      q.Collection('articles'),
+      { data: { testField: 'testValue' } }
+    )
+  )
+  .then(response => {
+    return{
+      statusCode: 200,
+      body: JSON.stringify(response)
+  }
+  })
+  .catch((error) => {
+      console.log('error', error)
+      return {
+        statusCode: 400,
+        body: JSON.stringify(error)
+      }
+    })
+}
--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 function App() {
-  console.log(process.env);
+  const [fetched, setFetch] = useState('');
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={async () => {
+          const response = await fetch('/.netlify/functions/createArticle')
+          .then(response => response.json())
+
+          setFetch(JSON.stringify(response));
+        }}>Create</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>{fetched}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
